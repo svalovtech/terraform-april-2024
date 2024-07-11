5a1b4d2df0fc31bdc3b6af0b1a83abd39a3ea98e
svalovtech <svalovtech@gmail.com>
2024-07-11 01:59:47 +0000

    new file

--- class4/main.tf
+++ class4/main.tf
@@ -5,25 +5,27 @@ provider aws {
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  availability_zone = var.kaizen
 }
 
+variable kaizen {}
+
 variable ami_id {
-    description = "Provide ami id"
+  description = "Provide ami id"
+  type = string
+  default = ""
+}
+
+variable instance_type {
+    description = "Provide instance type"
     type = string
     default = ""
 }
 
-variable instance_type {
-  description = "Provide instance type"
-  type = string
-  default = ""
-}
-
 variable region {
-  description = "Provide region"
-  type = string
-  default = ""
+    description = "Provide region"
+    type = string
+    default = ""
 }
 
-
-#ami-08be1e3e6c338b037
+# ami-08be1e3e6c338b037
